429cabf995a5c93bdde0117a11ab8a58b7952646
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 21:31:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,3 @@
 var playlist = new Object({NoDoubt: "Spiderwebs"})
-function updatePlaylist(playList, artistName, songTitle){playlist.artistName = songTitle; return playlist}
-function removeFromPlaylist(playlist, artistName){delete playlist.artistName; return playlist}
+function updatePlaylist(playList, artistName, songTitle){playlist[artistName] = songTitle; return playlist}
+function removeFromPlaylist(playlist, artistName){delete playlist[artistName]; return playlist}
